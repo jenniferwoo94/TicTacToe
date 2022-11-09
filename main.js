b7792c20265a6e6dffc1b66d3de48b9290264c6e
jenniferwoo94 <jenniferwoo94@gmail.com>
2022-11-09 13:16:48 -0600

    game can handle each players move

--- main.js
+++ main.js
@@ -24,15 +24,35 @@ let board, winner, turn;
 /* ----- Cached Element References/Variables ----- */
 // creating variables for DOM elements on the page that we're going to interact with more than once
 const squareEls = document.querySelectorAll('td');
-const turnEls = document.querySelector('turn');
 
 /* ----- Event Listeners ----- */
 document.querySelector('table').addEventListener('click', handleMove);
 
 /* ----- Functions ----- */
+// initialize the board when the page loads
+initialize();
+
 // initialize all our state variables, sets the stage for our application
 function initialize () {
     board = [null, null, null, null, null, null, null, null, null];
     turn = 1; 
     winner = null;
+    render();
+}
+
+function render() {
+    // run this function every time a player has made a move to render onto the DOM
+}
+
+function handleMove(evt) {
+    // grab the value from the square clicked
+    const idx = parseInt(evt.target.id.replace("sq", ''));
+    // check if the square has been clicked on
+    if (board[idx]) return;
+    // update the state variables
+    board[idx] = turn;
+    // next player's turn
+    turn *= -1;
+    // render the state variable changes to the DOM
+    // render()
 }
