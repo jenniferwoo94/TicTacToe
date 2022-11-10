2a9955e5f07d4bfabdac67829d5957ef6e71d632
jenniferwoo94 <jenniferwoo94@gmail.com>
2022-11-10 11:10:52 -0600

    Change the board style and the X's and O's.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /* ----- Constant Variables -----*/
 const lookup = {
-    "1": "pink",
-    "-1": "black",
-    null: "white",
+    "1": "X",
+    "-1": "O",
+    null: "",
 };
 
 const winningCombos = [
@@ -24,9 +24,11 @@ let board, winner, turn;
 /* ----- Cached Element References/Variables ----- */
 // creating variables for DOM elements on the page that we're going to interact with more than once
 const squareEls = document.querySelectorAll('td');
+const playerEl = document.getElementById('playerTurn');
 
 /* ----- Event Listeners ----- */
 document.querySelector('table').addEventListener('click', handleMove);
+document.querySelector('button').addEventListener('click', initialize);
 
 /* ----- Functions ----- */
 // initialize the board when the page loads
@@ -34,15 +36,29 @@ initialize();
 
 // initialize all our state variables, sets the stage for our application
 function initialize () {
-    board = [null, null, null, null, null, null, null, null, null];
+    board = [null, null, null,
+            null, null, null,
+            null, null, null];
+            /* Game grid is represented in the array as follows:
+            [0] [1] [2]
+            [3] [4] [5]
+            [6] [7] [8]
+            */
     turn = 1; 
     winner = null;
     render();
 }
 
+// run this function everyt time a player has made a move to render onto the DOM
 function render() {
-    // run this function every time a player has made a move to render onto the DOM
-}
+    // board variable index number-wise should correspond exactly with the board array
+    board.forEach(function(sq, idx) {
+    // sq represents the value (each one of the null values in board variable)
+    // idx represents the index value (both the board variable and the squareEls variable)
+        squareEls[idx].innerHTML = lookup[sq];
+    });
+
+};
 
 function handleMove(evt) {
     // grab the value from the square clicked
@@ -54,5 +70,5 @@ function handleMove(evt) {
     // next player's turn
     turn *= -1;
     // render the state variable changes to the DOM
-    // render()
+    render();
 }
